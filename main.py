54e321b37b303536795a39c65634e7e2ebd73846
Egisto Parronchi Neto <ticoparronchi@gmail.com>
2023-07-31 15:21:08 -0300

    feat: get pdf strings

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import pdfreader
 import argparse
 import glob
-from pdfreader import PDFDocument
+from pdfreader import SimplePDFViewer
+
+
+def generateCsvFile(filesPaths):
+    print()
+    for path in filesPaths:
+        fd = open(path, "rb")
+        viewer = SimplePDFViewer(fd)
+        for canvas in viewer:
+            pageStrings = canvas.strings
+            print(pageStrings)
+
+
+def getFilesPaths(filesFolderPath):
+    paths = glob.glob(filesFolderPath + "*.pdf")
+    if paths:
+        print("Processing your bills")
+    else:
+        print("The folder do not have any pdf file or the path is incorrect!")
+
+    return paths
 
 
 def getArgs():
     argParser = argparse.ArgumentParser()
-    argParser.add_argument("-p", "--path", help="path to your bills folder")
+    argParser.add_argument(
+        "-p", "--path", help="path to your bills (*.pdf) folder")
     args = argParser.parse_args()
 
     return args
@@ -14,8 +35,12 @@ def getArgs():
 
 def main():
     args = getArgs()
-    print("path=%s" % args.path)
-    print(glob.glob(args.path + "*.pdf"))
+    paths = getFilesPaths(args.path)
+
+    if not paths:
+        return
+
+    generateCsvFile(paths)
 
 
 if __name__ == '__main__':
