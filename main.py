d7472e67932a707fa93996807b10836d01fcaaf2
Egisto Parronchi Neto <ticoparronchi@gmail.com>
2023-08-01 18:50:22 -0300

    feat: finish generation csv table

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import argparse
 import glob
 import easyocr
 import os
-from PIL import Image
+import csv
+import time
 from pdf2image import convert_from_path
 
 
@@ -12,13 +13,12 @@ def cropImageForFastProcess(image, count):
     right = image.width - 1800
     bottom = image.height / 3.7
 
-
     image.crop((left, top, right, bottom)).save(
         f'image_{count}.png', 'PNG')
 
 
 def clearImage(imagePath):
-    print("Clearing old image")
+    print("Deleting old image")
     if os.path.exists(imagePath):
         try:
             os.remove(imagePath)
@@ -28,25 +28,49 @@ def clearImage(imagePath):
             print("Error code:", e.code)
 
 
-def generateCsvFile(filesPaths):
+def getBillsInfo(filesPaths):
     count = 0
     reader = easyocr.Reader(['pt'], verbose=False, gpu=False, quantize=True)
+    results = []
     for path in filesPaths:
         imageName = f'image_{count}.png'
         imagePath = './' + imageName
 
         clearImage(imagePath)
 
+        print("Converting PDF to image")
         images = convert_from_path(
             path, poppler_path=r"C:\Users\tico\Downloads\Release-23.07.0-0\poppler-23.07.0\Library\bin", first_page=1, last_page=1, dpi=800, grayscale=True)
 
+        print("Cropping image for better usage")
         cropImageForFastProcess(images[0], count)
 
-        result = reader.readtext(imagePath, detail = 0)
+        print("Extracting information form the bill image")
+        result = reader.readtext(imagePath, detail=0)
 
-        print(result)
+        results.append(result)
 
         count += 1
+
+    return results
+
+
+def writeCsvFile(results):
+    print("Generating table with light bills data")
+
+    header = ['Data', 'Consumption', 'Value']
+
+    f = open("./billsTable.csv", "w+")
+
+    writer = csv.writer(f)
+    writer.writerow(header)
+
+    for result in results:
+        writer.writerow([result[5], result[19], result[4]])
+
+    print("DONE! :)")
+
+    f.close()
 
 
 def getFilesPaths(filesFolderPath):
@@ -69,13 +93,21 @@ def getArgs():
 
 
 def main():
+    start = time.time()
     args = getArgs()
     paths = getFilesPaths(args.path)
 
     if not paths:
         return
 
-    generateCsvFile(paths)
+    results = getBillsInfo(paths)
+
+    writeCsvFile(results)
+
+    end = time.time()
+    
+    print('it took ' + time.strftime('%H:%M:%S', time.gmtime(end - start)) + ' to get your light data, use a computer with GPU for better performance')
+
 
 
 if __name__ == '__main__':
