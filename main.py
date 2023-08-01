0c0e9b406905da922735303926e36a71ce96524f
Egisto Parronchi Neto <ticoparronchi@gmail.com>
2023-08-01 20:25:37 -0300

    feat: change crop system

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 import glob
 import easyocr
+import numpy as np
+import cv2
 import os
 import csv
 import time
@@ -8,13 +10,36 @@ from pdf2image import convert_from_path
 
 
 def cropImageForFastProcess(image, count):
-    left = image.width / 2
-    top = 750
-    right = image.width - 1800
-    bottom = image.height / 3.7
-
-    image.crop((left, top, right, bottom)).save(
+    image.save(
         f'image_{count}.png', 'PNG')
+    imageCv2 = cv2.imread(f'image_{count}.png')
+    # Load image, convert to grayscale, and find edges
+    gray = cv2.cvtColor(imageCv2, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.adaptiveThreshold(
+        gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 51, 9)
+
+    cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+    for c in cnts:
+        cv2.drawContours(thresh, [c], -1, (255, 255, 255), -1)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=4)
+
+    # Draw rectangles, the 'area_treshold' value was determined empirically
+    cnts = cv2.findContours(opening, cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    area_treshold = 4000
+    count2 = 0
+    for c in cnts:
+        if cv2.contourArea(c) > area_treshold:
+            x, y, w, h = cv2.boundingRect(c)
+            image.crop((x, y, x + w, y + h)).save(
+                f'image_{count}_{count2}.png', 'PNG')
+        count2 += 1
+    print(cnts)
 
 
 def clearImage(imagePath):
@@ -46,9 +71,12 @@ def getBillsInfo(filesPaths):
         cropImageForFastProcess(images[0], count)
 
         print("Extracting information form the bill image")
-        result = reader.readtext(imagePath, detail=0)
+        result = reader.readtext(
+            imagePath, detail=0, batch_size=10, height_ths=10)
 
         results.append(result)
+
+        print(result)
 
         count += 1
 
@@ -105,9 +133,9 @@ def main():
     writeCsvFile(results)
 
     end = time.time()
-    
-    print('it took ' + time.strftime('%H:%M:%S', time.gmtime(end - start)) + ' to get your light data, use a computer with GPU for better performance')
 
+    print('it took ' + time.strftime('%H:%M:%S', time.gmtime(end - start)) +
+          ' to get your light data, use a computer with GPU for better performance')
 
 
 if __name__ == '__main__':
